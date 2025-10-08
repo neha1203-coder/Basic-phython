36af5ec1c644aeb894e7273980cb7ff86680c110
neha1203-coder <nehaskumari1203@gmail.com>
2025-10-08 21:49:49 +0530

    initial commit

---
+++ array/bubblesort.py
@@ -0,0 +1,7 @@
+# 13. Bubble sort
+arr = [int(x) for x in input().split()]
+for i in range(len(arr)):
+    for j in range(len(arr)-i-1):
+        if arr[j] > arr[j+1]:
+            arr[j], arr[j+1] = arr[j+1], arr[j]
+print("Sorted =", arr)

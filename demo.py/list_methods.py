691a632f6997378727be317451f0cdf9b33a65f3
neha1203-coder <nehaskumari1203@gmail.com>
2026-01-09 20:18:38 +0530

    initial commit

--- demo.py/list_methods.py
+++ demo.py/list_methods.py
@@ -6,7 +6,9 @@ list.sort()               # arrange the list in asecending order
 print(list)
 list.sort(reverse=True)    # arrange the list in decending order
 print(list)
-list.reverse()
+list.reverse()           # reverse the list
+print(list)
+list.insert(2,49)        # insert element at index  list.insert(index,element)
 print(list)
 fruits = ["banana", "apple", "ice apple", "orange", "carrot"]
 print(fruits)
@@ -17,4 +19,6 @@ print(fruits)
 fruits.sort(reverse=True)
 print(fruits)
 fruits.reverse()
+print(fruits)
+fruits.insert(3,"litchi")
 print(fruits)

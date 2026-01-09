ad2e39a8d97c9e4dc68a9b8e6e6b4a220212b977
neha1203-coder <nehaskumari1203@gmail.com>
2026-01-09 19:48:31 +0530

    initial commit

---
+++ array/conditional_statement.py
@@ -0,0 +1,16 @@
+
+age = int(input("enter age: "))
+if age>=18:
+    print("eligible to vote")
+else:
+    print("not eligible to vote")
+
+    age1 = int(input("enter age: "))
+    if age>=18: 
+        print("eligible for driving")
+    else:
+        print("not eligible for driving")
+     
+
+    
+
---
+++ demo.py/list.py
@@ -0,0 +1,17 @@
+marks = [23,43.4,34.3,54,32,64.90]
+student = ["karan", "shashank",43,59,"priya"]
+print(marks)
+print(student)
+print(type(marks))
+print(type(student))
+print(marks[0])
+print(marks[2])
+print(marks[4])
+print(marks[5])
+print(len(marks))
+
+#mutable condition
+print(student[3])
+student[3]="shubham"
+print(student)
+print(student[1:4])  #slicing and in slicing ending index is not included but starting index is included

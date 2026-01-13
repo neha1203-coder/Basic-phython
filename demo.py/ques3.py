2b3901c7e7c1af90ad004b7e449c47bcf5b1f857
neha1203-coder <nehaskumari1203@gmail.com>
2026-01-13 23:32:41 +0530

    initial commit

--- demo.py/ques3.py
+++ demo.py/ques3.py
@@ -1,7 +1,7 @@
-#grade = ("C","D","A","A","B","B","A")
-#grade.count("A")
-#print(grade)
-
-grade = ["C", "D", "A", "A", "B", "B","A"]
-grade.sort()
+grade = ("C","D","A","A","B","B","A")
+grade.count("A")
 print(grade)
+
+#grade = ["C", "D", "A", "A", "B", "B","A"]
+#grade.sort()
+#print(grade)

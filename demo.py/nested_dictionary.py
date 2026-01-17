9a1a4de1aba7f5c7c46544f8924fcfc8c44db39e
neha1203-coder <nehaskumari1203@gmail.com>
2026-01-17 16:39:43 +0530

    initial commit

---
+++ demo.py/nested_dictionary.py
@@ -0,0 +1,11 @@
+student = {
+    "name" : "shashank ranjan",
+    "subject" : {
+        "phy" : 94,
+        "maths" : 83,
+        "chem" : 78,
+    }
+}
+print(student)
+print(student["subject"])
+print(student["subject"]["chem"])

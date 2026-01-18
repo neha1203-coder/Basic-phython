240f9896dafb91477b1c8d2251aa85365a73aeb4
neha1203-coder <nehaskumari1203@gmail.com>
2026-01-18 15:17:55 +0530

    initial commit

--- demo.py/for_loop_ques.py
+++ demo.py/for_loop_ques.py
@@ -6,7 +6,7 @@ for i in range(1,11) :   #range() : range function returns a sequence of numbers
 
 
 #ques 1: prit numbers form 1 to 100
-for i in range(1,100,1):
+for i in range(1,101,1):
   print(i)
 
 #ques 2: print numbers form 100 to 1
@@ -26,6 +26,46 @@ for i in range(2,101,2):
   if(i<=20):
     break
   print(i)
+
+
+# pass statement : pass is a null statement that does nothing. It is used as a placeholder for future code.
+  for i in range(5):
+    pass
+  print("some useful work")
+ 
+ 
+# pass can use in if statement also
+  if(i<200):
+    pass
+  print("end of the code")
+
+
+#ques 5: wap to find the sum of the first n natural number.(using for loop) 
+n=int(input("enter a number: "))
+sum = 0
+for i in range(1,n+1):
+   sum +=1
+print(sum)
+
+#ques 6: wap to find the sum of the first n natural number.(using while loop)
+b = int(input("enter a number"))
+sum = 0
+i = 1
+while(i<=b):
+   sum += i
+i+=1
+print(sum)
+
+
+#ques 7: wap to find the factorial of first n number (using for loop)
+num1=int(input("enter a number"))
+fact = 1
+i = 1
+for i in range(1,num1+1):
+   fact*=i
+print(fact)   
+
+
 
 
 
